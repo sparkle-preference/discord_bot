a5b765fb368412decbf43440c0831005ebd1442d
tesence <thomasesence@gmail.com>
2017-12-25 18:50:27 +0100

    Change extension loading process

--- discord_bot/client.py
+++ discord_bot/client.py
@@ -1,12 +1,12 @@
 import logging
-import pkgutil
 
 from discord.ext import commands
 
-from discord_bot import cogs
 from discord_bot import utils
 
 LOG = logging.getLogger('debug')
+
+INITIAL_EXTENSIONS = ['stream', 'sr']
 
 
 class Bot(commands.Bot):
@@ -19,14 +19,15 @@ class Bot(commands.Bot):
 
     # BOT ACTIONS #
     def load_extensions(self):
-        """ Load all the cogs """
+        """ Load all the extensions """
         project_name = utils.get_project_name()
-        cog_module_name = "{project_name}.cogs".format(project_name=project_name)
-        cog_name_list = [(name, ispkg) for importer, name, ispkg in pkgutil.iter_modules(cogs.__path__)]
-        for cog_name, ispkg in cog_name_list:
-            if not ispkg:
-                self.load_extension(cog_module_name + "." + cog_name)
-                LOG.debug("The cog '{cog_name}' has been successfully loaded".format(cog_name=cog_name))
+        extension_module_name = "{project_name}.cogs".format(project_name=project_name)
+        for extension in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(extension_module_name + "." + extension)
+                LOG.debug("The extension '{extension}' has been successfully loaded".format(extension=extension))
+            except:
+                LOG.error("Failed to load extension {extension}".format(extension=extension))
 
     async def say(self, ctx, message):
         message = "```" + message + "```"
